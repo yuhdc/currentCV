7a0fcc20894a8b37a221a4f370701cad98744018
yuhdc <74478717+yuhdc@users.noreply.github.com>
2021-07-25 16:20:54 +0700

    Updates

---
+++ precache-manifest.c49fa15f10178844055530c096bf7674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2c065ecfcad5e7943a8cb1f4d8f579",
+    "url": "/currentCV/index.html"
+  },
+  {
+    "revision": "c0636af8d164f2aee9e3",
+    "url": "/currentCV/static/css/2.9e3fc118.chunk.css"
+  },
+  {
+    "revision": "ac82eecb1c4d330366b3",
+    "url": "/currentCV/static/css/main.126ea473.chunk.css"
+  },
+  {
+    "revision": "c0636af8d164f2aee9e3",
+    "url": "/currentCV/static/js/2.c3292650.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/currentCV/static/js/2.c3292650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac82eecb1c4d330366b3",
+    "url": "/currentCV/static/js/main.c86926c8.chunk.js"
+  },
+  {
+    "revision": "8d2c174160e8fa628fc9",
+    "url": "/currentCV/static/js/runtime-main.0bfcf756.js"
+  }
+]);
