dcd9bfb1e194acd779dedd3c577ba356d59fd697
yuhdc <74478717+yuhdc@users.noreply.github.com>
2021-07-25 16:10:03 +0700

    deployment

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+window.$primaryLanguage = "en";
+window.$secondaryLanguage = "vn";
+window.$primaryLanguageIconId = "primary-lang-icon";
+window.$secondaryLanguageIconId = "secondary-lang-icon";
+
+ReactDOM.render(<App />, document.getElementById("root"));
